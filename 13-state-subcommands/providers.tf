9634344421109dae07dc4062f369107e7fb2e69d
Michael Brister <mike@mikebrister.com>
2026-01-25 21:13:42 -0500

    Update provider endpoints

--- 13-state-subcommands/providers.tf
+++ 13-state-subcommands/providers.tf
@@ -16,7 +16,17 @@ provider "aws" {
   skip_requesting_account_id  = true
   skip_metadata_api_check     = true
   endpoints {
-    s3  = "http://localhost:4566"
-    sts = "http://localhost:4566"
+    s3             = "http://localhost:4566"
+    sts            = "http://localhost:4566"
+    ec2            = "http://localhost:4566"
+    dynamodb       = "http://localhost:4566"
+    sqs            = "http://localhost:4566"
+    sns            = "http://localhost:4566"
+    lambda         = "http://localhost:4566"
+    apigateway     = "http://localhost:4566"
+    cloudwatch     = "http://localhost:4566"
+    iam            = "http://localhost:4566"
+    kinesis        = "http://localhost:4566"
+    secretsmanager = "http://localhost:4566"
   }
 }
